38fceaa351b8ae95e24fa5b874b0f187b13b5692
BaronVice <toni.sas.228@mail.ru>
2023-03-13 11:29:48 +0800

    structure plan to build it

--- src/main/java/baronvice/springstuff/musicplayer/IMusicPlayer.java
+++ src/main/java/baronvice/springstuff/musicplayer/IMusicPlayer.java
@@ -3,4 +3,5 @@ package baronvice.springstuff.musicplayer;
 public interface IMusicPlayer {
     void playSong(String name);
     void pauseSong();
+    void continueSong();
 }
--- src/main/java/baronvice/springstuff/musicplayer/MusicPlayer.java
+++ src/main/java/baronvice/springstuff/musicplayer/MusicPlayer.java
@@ -1,11 +1,14 @@
 package baronvice.springstuff.musicplayer;
 
-/** So concept is: while song is played print lyrics line every second. Probably should do it with threads.
+import lombok.SneakyThrows;
+
+/** Concept is: while song is played print lyrics line every second. Probably should do it with threads.
  * When I want it to pause .await() more likely */
 
 public class MusicPlayer implements IMusicPlayer {
     private Music music;
     private Song currentSong;
+    private Thread thread;
     public MusicPlayer(Music music){
         this.music = music;
     }
@@ -13,11 +16,26 @@ public class MusicPlayer implements IMusicPlayer {
     @Override
     public void playSong(String name) {
         currentSong = music.getSong(name);
-        System.out.println(currentSong == null ? "No match" : currentSong);
+        if (currentSong == null){
+            System.out.println("No match");
+            return;
+        }
+
+        thread = new Thread(this::startSong);
+    }
+
+    public void startSong(){
+
+    }
+
+    @SneakyThrows
+    @Override
+    public void pauseSong() {
+        thread.wait();
     }
 
     @Override
-    public void pauseSong() {
-        // .await()
+    public void continueSong(){
+        thread.notifyAll();
     }
 }
